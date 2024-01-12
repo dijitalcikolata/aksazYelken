30bb2a213220108f95682c39feea536dfc9dfc81
dijitalcikolata <dijitalcikolata@icloud.com>
2024-01-12 23:32:06 +0300

    Initial Commit

---
+++ aksazYelken/aksazYelkenApp.swift
@@ -0,0 +1,17 @@
+//
+//  aksazYelkenApp.swift
+//  aksazYelken
+//
+//  Created by MURAT HASIRCI on 12.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct aksazYelkenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
